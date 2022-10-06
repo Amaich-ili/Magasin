d31d4e4d8eb586f6204496cfbb8182e9752d8500
Iliass <iliamaich@gmail.com>
2022-10-06 10:51:28 -0400

    mon progamme

--- Iliass_lab4.py
+++ Iliass_lab4.py
@@ -8,10 +8,8 @@ Session été 2022
 class Magasin:
     def __init__(self):
         self.liste_objet = []
-        # self.dict_jeu = {}
         self.choix = ""
         self.qtt = int
-        # self.liste_jeu = []
         with open("fichier.txt", "r") as f:
             lines = f.readlines()
             for line in lines:
@@ -26,13 +24,6 @@ class Magasin:
                     self.liste_objet.append(Jeu_assemblage(objet[1], int(objet[2])))
                 elif objet[0] == "Jeu_adresse":
                     self.liste_objet.append(Jeu_adresse(objet[1], int(objet[2])))
-
-    # def lire_file(self):
-    #     with open("fichier.txt", "r") as f:
-    #         lines = f.readlines()
-    #         for line in lines:
-    #             objet = line.split()
-    #             self.dict_jeu[objet[1]] = int(objet[2])
 
     def inventaire(self):
         for objet in self.liste_objet:
@@ -151,7 +142,6 @@ class Jeu_adresse(Jeu):
         self.quantite = quantite
 
 class tournoi:
-    # liste_condidat = []
     def __init__(self, nom, courriel, date):
         self.nom = nom
         self.courriel = courriel
@@ -165,70 +155,8 @@ class Pochette:
         return (f"Voici votre pochette plastifiante {self.couleur}")
 
 
-
-
-# def menu():
-#     magasin = Magasin()
-#     condition = True
-
-#     while condition:
-#         print("\n\t--Binvenue dans note magasin de jeux--")
-#         print("\nVoici les jeux disponibles")
-#         choix = input("\n(1). Acheter un jeu "
-#                       "\n(2). Voir l'inventaire  "
-#                       "\n(3). jeux de rôle "
-#                       "\n(4). Jeux d'assemblage "
-#                       "\n(5). Jeux d'adresse "
-#                       "\n(6). Afficher tous les jeux disponibles"
-#                       "\n(6). Quiter"
-#                       "\nVotre choix : ")
-
-#         if choix == "1":
-#             for objet in magasin.liste_objet:
-#                 print(objet)
-#         elif choix == "2":
-#             print(Jeu_strategie())
-#         elif choix == "3":
-#             print(Jeur_role())
-#         elif choix == "4":
-#             print(Jeu_assemblage())
-#         elif choix == "5":
-#             print(Jeu_adresse())
-#         elif choix == "6":
-#             condition = False
-
-
-# menu()
 m = Magasin()
 m.menu()
-
-# je = Jeu_carte("kak",2)
-# print(je)
-
-
-
-
-                # if objet[0] == "Uno":
-                #     self.liste_jeu.append(Jeu_carte(objet[0], objet[1]))
-                # elif objet[0] == "Solitaire":
-                #     self.liste_jeu.append(Jeu_carte(objet[0], objet[1]))
-                # elif objet[0] == "Elvenar":
-                #     self.liste_jeu.append(Jeu_strategie(objet[0], objet[1]))
-                # elif objet[0] == "Grepolis":
-                #     self.liste_jeu.append(Jeu_strategie(objet[0], objet[1]))
-                # elif objet[0] == "malefices":
-                #     self.liste_jeu.append(Jeur_role(objet[0], objet[1]))
-                # elif objet[0] == "Kuro":
-                #     self.liste_jeu.append(Jeur_role(objet[0], objet[1]))
-                # elif objet[0] == "Casse_tete":
-                #     self.liste_jeu.append(Jeu_assemblage(objet[0], objet[1]))
-                # elif objet[0] == "Rubik's_cube":
-                #     self.liste_jeu.append(Jeu_assemblage(objet[0], objet[1]))
-                # elif objet[0] == "Monopolie":
-                #     self.liste_jeu.append(Jeu_adresse(objet[0], objet[1]))
-                # elif objet[0] == "mahjong":
-                #     self.liste_jeu.append(Jeu_adresse(objet[0], objet[1]))
-
 
 
 
